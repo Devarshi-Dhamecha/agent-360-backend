3dcb24d6af29cb11ac70c9fb7d83ad2c2660f747
ParthShindenovus <parth.s@novuscode.com>
2026-02-23 18:43:26 +0530

    chore(config): Add production domain to CORS allowed origins
    - Add https://red-coyote-805490.hostingersite.com to CORS_ALLOWED_ORIGINS
    - Enable cross-origin requests from production environment
    - Maintain existing localhost development origins

--- config/settings.py
+++ config/settings.py
@@ -84,6 +84,7 @@ SPECTACULAR_SETTINGS = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://red-coyote-805490.hostingersite.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
